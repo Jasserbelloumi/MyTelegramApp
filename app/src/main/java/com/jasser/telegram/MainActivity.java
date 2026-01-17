61c68393db9bf30dc3bd140201b37710ffa59a60
Jasserbelloumi <jasser@example.com>
2026-01-17 15:27:32 +0100

    Fix syntax error in MainActivity

--- app/src/main/java/com/jasser/telegram/MainActivity.java
+++ app/src/main/java/com/jasser/telegram/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    protected void Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
